aac351a3b88ee02a6d006771f3d69d7c7c2ea1a5
Joseph Santantonio <js958@njit.edu>
2023-03-10 17:06:16 -0500

    Initial commit

---
+++ shop-calculator/README.md
@@ -0,0 +1,2 @@
+# Shop Calculator
+A calculator that will help you find the cost of purchasing X amount of items from a shop per world.
---
+++ shop-calculator/src/main/java/com/shopCalc/shopCalc/ShopCalculatorConfig.java
@@ -0,0 +1,19 @@
+package com.shopCalc.shopCalc;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("shopcalculator")
+public interface ShopCalculatorConfig extends Config
+{
+	@ConfigItem(
+		keyName = "greeting",
+		name = "Welcome Greeting",
+		description = "The message to show to the user when they login"
+	)
+	default String greeting()
+	{
+		return "Hello";
+	}
+}
